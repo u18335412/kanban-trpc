a25beb60edd2414cbe392b16e1012166363cb89d
Awelani Murovhi <u18335412@tuks.co.za>
2022-08-05 01:48:24 +0200

    extracted column logic to external Column compnent

--- src/pages/column.tsx
+++ src/pages/column.tsx
@@ -2,8 +2,9 @@ import { trpc } from '../utils/trpc';
 import { useEffect, useState } from 'react';
 import { NextPageWithLayout } from './_app';
 import { TbLayoutBoardSplit } from 'react-icons/tb';
-import Task from '~/components/Task';
 import ViewTaskModal from '~/components/ViewTaskModal';
+import { ReactSortable } from 'react-sortablejs';
+import Column from '~/components/Column';
 
 const IndexPage: NextPageWithLayout = () => {
   const utils = trpc.useContext();
@@ -11,6 +12,7 @@ const IndexPage: NextPageWithLayout = () => {
   const columns = trpc.useQuery(['column.all']);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [selectedTaskId, setSelectedTaskId] = useState<string>('');
+  const [columnsState, setColumns] = useState(columns?.data || []);
   // const addPost = trpc.useMutation('post.add', {
   //   async onSuccess() {
   //     // refetches posts after a post is added
@@ -58,40 +60,19 @@ const IndexPage: NextPageWithLayout = () => {
           <div>
             <h2>{boards.data?.[0] && <>{boards.data[0].title}</>}</h2>
           </div>
-          <div className="flex mt-4 gap-x-4 overscroll-x-auto">
-            {columns.data?.map(({ task, title, id }) => {
-              return (
-                <div key={id} className=" w-60">
-                  <p>
-                    {title} ({task.length})
-                  </p>
-                  <ul className="flex flex-col mt-2 gap-y-3">
-                    {task.map(({ title, id, Sub_Task }) => {
-                      return (
-                        <li
-                          key={id}
-                          onClick={() => {
-                            setSelectedTaskId(id);
-                            setIsOpen(true);
-                          }}
-                        >
-                          <Task
-                            id={id}
-                            title={title}
-                            subtask_length={Sub_Task.length}
-                            subtasks_completed={
-                              Sub_Task.filter((task) => task.complete === true)
-                                .length
-                            }
-                          />
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              );
+          <ReactSortable
+            list={columnsState}
+            setList={setColumns}
+            group={boards.data?.[0]?.title}
+            animation={200}
+            delay={2}
+            handle=".column-handle"
+            className="flex mt-4 gap-x-4 overscroll-x-auto"
+          >
+            {columnsState.map((column) => {
+              return <Column column={column} key={column.id} />;
             })}
-          </div>
+          </ReactSortable>
         </div>
       </div>
     </>
