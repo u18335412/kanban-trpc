cb1100b81576711f43696eda9a241b432f4a8565
Awelani Murovhi <u18335412@tuks.co.za>
2022-08-15 21:21:13 +0200

    Transition has default values

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -3,20 +3,34 @@ import { Transition } from '@headlessui/react';
 
 const TransitionChild: FC<{
   children: ReactNode;
-  isOpen: boolean;
-}> = ({ children }) => {
+  isOpen?: boolean;
+  enter?: string;
+  enterFrom?: string;
+  enterTo?: string;
+  leave?: string;
+  leaveFrom?: string;
+  leaveTo?: string;
+}> = ({
+  children,
+  enter = 'ease-out duration-300',
+  enterFrom = 'opacity-0 scale-95',
+  enterTo = 'opacity-100 scale-100',
+  leave = 'ease-in duration-200',
+  leaveFrom = 'opacity-100 scale-100',
+  leaveTo = 'opacity-0 scale-95',
+}) => {
   return (
-    <Transition
+    <Transition.Child
       as={Fragment}
-      enter="ease-out duration-300"
-      enterFrom="opacity-0 scale-95"
-      enterTo="opacity-100 scale-100"
-      leave="ease-in duration-200"
-      leaveFrom="opacity-100 scale-100"
-      leaveTo="opacity-0 scale-95"
+      enter={enter}
+      enterFrom={enterFrom}
+      enterTo={enterTo}
+      leave={leave}
+      leaveFrom={leaveFrom}
+      leaveTo={leaveTo}
     >
       {children}
-    </Transition>
+    </Transition.Child>
   );
 };
 
