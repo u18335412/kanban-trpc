4f7bbe6f3eefeeb5a67478138aa2e05dfa6b2829
Awelani Murovhi <u18335412@tuks.co.za>
2022-08-15 21:21:39 +0200

    extracted transition

--- src/components/ViewTaskModal.tsx
+++ src/components/ViewTaskModal.tsx
@@ -3,6 +3,7 @@ import { Fragment, FC } from 'react';
 import { trpc } from '~/utils/trpc';
 import useAppStore from '~/data/useStore';
 import { BsThreeDotsVertical } from 'react-icons/bs';
+import TransitionChild from './Transition';
 
 const ViewTaskModal: FC<{
   closeModal: () => void;
@@ -15,8 +16,7 @@ const ViewTaskModal: FC<{
     <>
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          <Transition.Child
-            as={Fragment}
+          <TransitionChild
             enter="ease-out duration-300"
             enterFrom="opacity-0"
             enterTo="opacity-100"
@@ -25,19 +25,11 @@ const ViewTaskModal: FC<{
             leaveTo="opacity-0"
           >
             <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
+          </TransitionChild>
 
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex items-center justify-center min-h-full p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
+              <TransitionChild>
                 <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
                   {task.isLoading ? (
                     <div className="text-xl animate-pulse">Loading</div>
@@ -109,7 +101,7 @@ const ViewTaskModal: FC<{
                     </>
                   )}
                 </Dialog.Panel>
-              </Transition.Child>
+              </TransitionChild>
             </div>
           </div>
         </Dialog>
