3fe54dca3be0b6accd0b2ee572adcc59d29528d5
Awelani Murovhi <u18335412@tuks.co.za>
2022-09-13 22:23:52 +0200

    toggle side bar and theme

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,13 +1,16 @@
 import { FC, useEffect, useState } from 'react';
-import { TbLayoutBoardSplit } from 'react-icons/tb';
 import { AiOutlinePlus } from 'react-icons/ai';
-import { BiHide } from 'react-icons/bi';
 import { trpc } from '../utils/trpc';
 import useAppStore from '~/data/useStore';
 import { ModalType } from './ModalManager';
+import TransitionChild from './Transition';
+import { Transition } from '@headlessui/react';
+import ThemeToggle from './ThemeToggle';
+import useTheme from '~/data/useTheme';
 
 const SideBar: FC = () => {
-  const [hideSideBar, setHideSideBar] = useState<boolean>(false);
+  const { theme } = useTheme();
+  const [hideSideBar, setHideSideBar] = useState<boolean>(true);
   const { setSelectedBoard, selectedBoard, setSelectedModal } = useAppStore();
   const { isLoading, data } = trpc.useQuery(['board.all']);
 
@@ -23,41 +26,76 @@ const SideBar: FC = () => {
 
   if (isLoading) return <div className=" animate-pulse">Loading</div>;
 
+  if (!hideSideBar) {
+    return (
+      <button
+        onClick={() => setHideSideBar((prev) => !prev)}
+        className="absolute flex items-center justify-center h-12 rounded-r-full bottom-8 bg-main-purple w-14"
+      >
+        <img src="/assets/icon-show-sidebar.svg" alt="show sidebar" />
+      </button>
+    );
+  }
+
   return (
-    <>
-      {!hideSideBar && (
-        <aside className="flex flex-col py-4 min-w-[15rem]">
-          <div className="px-4">
-            <p className="text-xl font-bold">Kanban</p>
+    <Transition appear show={hideSideBar}>
+      <TransitionChild>
+        <aside className="flex flex-col pb-8 min-w-[18.75rem] h-full dark:bg-dark-grey">
+          <div className="flex items-center h-24 px-8">
+            {theme === 'dark' ? (
+              <img src="/assets/logo-light.svg" alt="logo light" />
+            ) : (
+              <img src="/assets/logo-dark.svg" alt="logo dark" />
+            )}
           </div>
-          <div className="mt-8">
-            <p className="px-4 uppercase">All boards({data?.length})</p>
-            <nav className="text-sm">
-              <ul className="flex flex-col mt-4 gap-y-2 ">
+          <div className="mt-4">
+            <p className="px-8 text-xs tracking-[0.15rem] font-bold uppercase text-medium-grey">
+              All boards({data?.length})
+            </p>
+            <nav className="font-bold text-md">
+              <ul className="flex flex-col mt-5">
                 {data?.map(({ id, title }) => {
                   return (
                     <li
                       onClick={() => updateSelectedBoard(id)}
                       key={id}
-                      className={`flex items-center cursor-pointer py-2 gap-x-2 px-4 ${
+                      className={`flex  group  mr-8 rounded-r-full transition-all items-center cursor-pointer py-4 gap-x-4 px-8 ${
                         selectedBoard === id
-                          ? 'bg-indigo-500 text-white transition-all mr-8 rounded-r-full'
-                          : ''
+                          ? ' bg-main-purple hover:bg-main-purple text-white transition-all '
+                          : 'hover:bg-white'
                       }`}
                     >
-                      <TbLayoutBoardSplit className="w-5 h-5" />
-                      <span className="w-full">{title}</span>
+                      <div
+                        className={` [mask:url('/assets/icon-board.svg') _no-repeat_center_/_contain]
+                        [-webkit-mask:url('/assets/icon-board.svg')no-repeat__center_/_contain]
+                        w-4 h-4 ${
+                          selectedBoard === id ? 'bg-white' : ' bg-medium-grey'
+                        }`}
+                      />
+                      <span
+                        className={`w-full  ${
+                          selectedBoard === id
+                            ? ' text-white '
+                            : 'text-medium-grey group-hover:text-main-purple'
+                        }`}
+                      >
+                        {title}
+                      </span>
                     </li>
                   );
                 })}
                 <li>
                   <button
                     onClick={() => setSelectedModal(ModalType.NewBoard)}
-                    className="flex items-center px-4 text-indigo-500 cursor-pointer gap-x-2"
+                    className="flex items-center px-8 py-4 text-indigo-500 cursor-pointer gap-x-4"
                   >
-                    <TbLayoutBoardSplit className="w-5 h-5" />
-                    <div className="flex items-center w-full">
-                      <AiOutlinePlus className="w-5 h-5" />
+                    <div
+                      className={`[mask:url('/assets/icon-board.svg') _no-repeat_center_/_contain]
+                        [-webkit-mask:url('/assets/icon-board.svg')no-repeat__center_/_contain]
+                        w-4 h-4 bg-main-purple`}
+                    />
+                    <div className="flex items-center w-full text-main-purple">
+                      <AiOutlinePlus className="w-4 h-4" />
                       <span className="w-full">Create New Board</span>
                     </div>
                   </button>
@@ -65,21 +103,34 @@ const SideBar: FC = () => {
               </ul>
             </nav>
           </div>
-          <div className="flex items-center px-4 mt-auto gap-x-2">
-            <div className="relative">
-              <BiHide className="pointer-events-none " />
-              <input
-                defaultChecked={hideSideBar}
-                type="checkbox"
-                className="absolute inset-0 w-full h-full appearance-none cursor-pointer"
-                onChange={() => setHideSideBar(!hideSideBar)}
-              />
+          <div className="mt-auto">
+            <ThemeToggle />
+            <div className="px-6 mt-5">
+              <div className="relative flex items-center gap-x-2 ">
+                <img
+                  className="w-4 h-4 "
+                  src="/assets/icon-hide-sidebar.svg"
+                  alt="hide side bar"
+                />
+                <input
+                  name="hideSideBar"
+                  defaultChecked={hideSideBar}
+                  type="checkbox"
+                  className="absolute inset-0 w-full h-full appearance-none cursor-pointer"
+                  onChange={() => setHideSideBar(!hideSideBar)}
+                />
+                <label
+                  htmlFor="hideSideBar"
+                  className="font-bold pointer-events-none text-medium-grey text-md"
+                >
+                  Hide Sidebar
+                </label>
+              </div>
             </div>
-            <span>Hide Sideabr</span>
           </div>
         </aside>
-      )}
-    </>
+      </TransitionChild>
+    </Transition>
   );
 };
 
