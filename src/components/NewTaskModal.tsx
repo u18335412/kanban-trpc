032e2c71aff9e8493ed35f01acc8f29c07868865
Awelani Murovhi <u18335412@tuks.co.za>
2022-09-14 19:53:06 +0200

    refactor

--- src/components/NewTaskModal.tsx
+++ src/components/NewTaskModal.tsx
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react';
-import React, { Fragment, FC } from 'react';
+import React, { Fragment, FC, useState, useEffect } from 'react';
 import { trpc } from '~/utils/trpc';
 import { useForm, useFieldArray, FieldValues } from 'react-hook-form';
 import * as zod from 'zod';
@@ -7,11 +7,15 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai';
 import TransitionChild from './Transition';
 import useAppStore from '~/data/useStore';
+import InputLabel from './InputLabel';
+import Input from './Input';
+import Button from './Button';
+import StatusSelect, { StatusItemsInterface } from './StatusSelect';
+import useTheme from '~/data/useTheme';
 
 const schema = zod.object({
   title: zod.string().min(1, { message: 'Required' }),
   description: zod.string(),
-  column_id: zod.string().min(1, { message: 'Required' }),
   sub_tasks: zod.array(
     zod.object({
       title: zod.string().min(1, { message: 'Required' }),
@@ -23,14 +27,24 @@ const NewTaskModal: FC<{
   closeModal: () => void;
   isOpen: boolean;
 }> = ({ closeModal, isOpen }) => {
+  const { theme } = useTheme();
   const utils = trpc.useContext();
   const { selectedBoard } = useAppStore();
   const { isLoading, data } = trpc.useQuery([
     'board.getColumns',
     { id: selectedBoard },
   ]);
+  const [selected, setSelected] = useState<StatusItemsInterface | undefined>({
+    value: data?.Column[0]?.id || '',
+    text: data?.Column[0]?.title || '',
+  });
   const mutation = trpc.useMutation(['task.add']);
-  const { register, handleSubmit, control } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm({
     resolver: zodResolver(schema),
   });
   const { fields, append, remove } = useFieldArray({
@@ -56,6 +70,15 @@ const NewTaskModal: FC<{
     );
   };
 
+  useEffect(() => {
+    if (data?.Column[0]) {
+      setSelected({
+        value: data?.Column[0]?.id,
+        text: data?.Column[0]?.title,
+      });
+    }
+  }, [data?.Column]);
+
   const addSubTask = (e: React.FormEvent) => {
     e.preventDefault();
     append({ title: '' });
@@ -73,105 +96,102 @@ const NewTaskModal: FC<{
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className="fixed inset-0 bg-black/50" />
           </TransitionChild>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex items-center justify-center min-h-full p-4 text-center">
+          <div className={`fixed inset-0 overflow-y-auto ${theme}`}>
+            <div className="flex items-center justify-center min-h-full text-center dark:text-white">
               <TransitionChild>
-                <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <Dialog.Panel className="w-full max-w-[30rem] p-8  text-left min-w-max align-middle transition-all dark:bg-dark-grey transform bg-white shadow-xl rounded-md">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="text-lg font-bold leading-6 text-gray-900"
                   >
                     Add New Task
                   </Dialog.Title>
 
-                  <form onSubmit={handleSubmit(handleFormSubmit)}>
-                    <div className="[&>*]:flex [&>*]:flex-col [&>*]:gap-1 flex flex-col gap-y-2">
+                  <form
+                    onSubmit={handleSubmit(handleFormSubmit)}
+                    className="mt-6"
+                  >
+                    <div className="[&>*]:flex [&>*]:flex-col [&>*]:gap-1 flex flex-col gap-y-6">
                       <div>
-                        <label htmlFor="title" className="">
-                          Title
-                        </label>
-                        <input
-                          {...register('title', { required: true })}
-                          type="text"
-                          name="title"
-                          className="p-2 rounded ring-1 ring-black/50"
-                          placeholder="e.g Take coffee break"
+                        <InputLabel label="Title" htmlFor="title" />
+                        <Input
+                          errors={errors}
+                          id="title"
+                          register={register}
+                          placeholder="e.g. Take coffee break"
                         />
                       </div>
                       <div>
-                        <label htmlFor="title" className="">
-                          Description
-                        </label>
-                        <textarea
-                          {...register('description', { required: true })}
-                          rows={4}
-                          name="description"
-                          className="p-2 rounded ring-1 ring-black/50"
-                          placeholder="e.g It's always good to take a break, this 15 minute break will recharge the batteries a little."
+                        <InputLabel label="Description" htmlFor="description" />
+                        <Input
+                          errors={errors}
+                          type="textarea"
+                          id="description"
+                          register={register}
+                          placeholder="e.g. It's always good to take a break, this 15 minute break will recharge the batteries a little."
                         />
                       </div>
                       <div className="flex">
-                        <p className="">Subtasks</p>
-                        <ul className="flex flex-col gap-y-2 ">
+                        <InputLabel label="Subtasks" htmlFor="Subtasks" />
+                        <ul className="flex flex-col mt-2 gap-y-2">
                           {fields.map(({ id }, index) => {
                             return (
                               <li
                                 key={id}
                                 className="flex items-center justify-between gap-x-2"
                               >
-                                <input
-                                  className="w-full p-2 rounded ring-black/50 ring-1"
-                                  type="text"
-                                  {...register(`sub_tasks.${index}.title`, {
-                                    required: true,
-                                  })}
+                                <Input
+                                  errors={errors}
+                                  id={`sub_tasks.${index}.title`}
+                                  register={register}
+                                  placeholder=""
                                 />
                                 <button
                                   arial-label="Remove task"
                                   onClick={() => remove(index)}
+                                  className="group"
                                 >
-                                  <AiOutlineClose className="bg-white" />
+                                  <AiOutlineClose className="transition-all bg-white group-hover:text-red" />
                                 </button>
                               </li>
                             );
                           })}
                         </ul>
                         <div className="mt-4">
-                          <button
-                            onClick={(e) => addSubTask(e)}
-                            className="flex items-center justify-center w-full p-2 text-indigo-500 bg-white rounded-full ring-2 ring-black/50 gap-x-2"
+                          <Button
+                            onClick={addSubTask}
+                            icon={<AiOutlinePlus />}
+                            className="flex justify-center w-full py-2 bg-white text-main-purple hover:bg-main-purple/25"
                           >
-                            <AiOutlinePlus /> Add New Sub
-                          </button>
+                            Add New Subtask
+                          </Button>
                         </div>
-                        <div className="mt-4">
-                          <p>Status</p>
-                          <select
-                            defaultValue={data?.Column[0] && data?.Column[0].id}
-                            {...register('column_id', { required: true })}
-                            className={`w-full p-2 mt-1 ring-2 ${
-                              isLoading && 'bg-gray-300 animate-pulse h-10'
-                            }`}
-                          >
-                            {data?.Column.map(({ id, title }) => {
-                              return (
-                                <option key={id} value={id.toString()}>
-                                  {title}
-                                </option>
-                              );
-                            })}
-                          </select>
+                        <div className="mt-6">
+                          <InputLabel label="Status" htmlFor="Subtasks" />
+                          <div className="mt-2">
+                            <StatusSelect
+                              statusItems={data?.Column.map(
+                                ({ id, title }) => ({
+                                  value: id,
+                                  text: title,
+                                }),
+                              )}
+                              selected={selected}
+                              setSelected={setSelected}
+                            />
+                          </div>
                         </div>
 
-                        <div className="mt-4">
-                          <input
+                        <div className="mt-6">
+                          <Button
                             type="submit"
-                            value="Create Task"
-                            className="flex items-center justify-center w-full p-2 text-white bg-indigo-500 rounded-full ring-2 gap-x-2"
-                          />
+                            className="flex justify-center w-full py-2"
+                          >
+                            Create Task
+                          </Button>
                         </div>
                       </div>
                     </div>
