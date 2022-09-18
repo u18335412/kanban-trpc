1bf32fdde3c0e7337e82ef4645a4b6e9ba55b77b
Awelani Murovhi <u18335412@tuks.co.za>
2022-09-18 11:11:56 +0200

    Added mutation for  updating column order

--- src/server/routers/board.ts
+++ src/server/routers/board.ts
@@ -7,17 +7,21 @@ import { prisma } from '~/server/prisma';
 const defaultboardSelect = Prisma.validator<Prisma.BoardSelect>()({
   id: true,
   title: true,
+  Column: true,
+  columnOrder: true,
 });
 
 export const boardRouter = createRouter()
   .mutation('add', {
     input: z.object({
       title: z.string().min(1).max(200),
-      columns: z.array(
-        z.object({
-          title: z.string().min(1).max(200),
-        }),
-      ),
+      columns: z
+        .array(
+          z.object({
+            title: z.string().min(1).max(200),
+          }),
+        )
+        .optional(),
     }),
     async resolve({ input }) {
       const { title, columns } = input;
@@ -77,9 +81,7 @@ export const boardRouter = createRouter()
       const { id } = input;
       const board = await prisma.board.findUnique({
         where: { id },
-        select: {
-          Column: true,
-        },
+        select: defaultboardSelect,
       });
       if (!board) {
         throw new TRPCError({
@@ -95,23 +97,69 @@ export const boardRouter = createRouter()
       id: z.string().min(1).max(200),
       data: z.object({
         title: z.string().min(1).max(200),
-        columns: z.array(
-          z.object({
-            title: z.string().min(1).max(200),
-          }),
-        ),
+        updateOrNewcolumns: z
+          .array(
+            z.object({
+              title: z.string().min(1).max(200),
+              id: z.string(),
+            }),
+          )
+          .optional(),
+        removeColumns: z
+          .array(
+            z.object({
+              title: z.string().min(1).max(200),
+              id: z.string().min(1).max(200),
+            }),
+          )
+          .optional(),
       }),
     }),
     async resolve({ input }) {
-      const { id, data } = input;
-      return prisma.board.update({
+      const {
+        id,
+        data: { title, updateOrNewcolumns, removeColumns },
+      } = input;
+      const board = await prisma.board.update({
         where: { id },
-        data,
+        data: {
+          title: title,
+        },
         select: defaultboardSelect,
       });
+
+      if (!board)
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: `Error while updating board with id:'${id}'`,
+        });
+
+      updateOrNewcolumns?.forEach(async (column) => {
+        await prisma.column.upsert({
+          where: {
+            id: column.id,
+          },
+          update: {
+            title: column.title,
+          },
+          create: {
+            title: column.title,
+            board_id: id,
+          },
+        });
+      });
+
+      await prisma.column.deleteMany({
+        where: {
+          id: {
+            in: removeColumns?.map((column) => column.id) || [],
+          },
+        },
+      });
+
+      return board;
     },
   })
-
   .mutation('delete', {
     input: z.object({
       id: z.string().uuid(),
@@ -119,6 +167,29 @@ export const boardRouter = createRouter()
     async resolve({ input }) {
       const { id } = input;
       await prisma.board.delete({ where: { id } });
+      return {
+        id,
+      };
+    },
+  })
+  .mutation('updateColumnOrder', {
+    input: z.object({
+      id: z.string().uuid(),
+      data: z.object({
+        columnOrder: z.array(z.string()),
+      }),
+    }),
+    async resolve({ input }) {
+      const {
+        id,
+        data: { columnOrder },
+      } = input;
+      await prisma.board.update({
+        where: { id },
+        data: {
+          columnOrder: columnOrder.toString(),
+        },
+      });
       return {
         id,
       };
