ec44e4e01531976c9b59014f9efe0452da51757a
Awelani Murovhi <u18335412@tuks.co.za>
2022-10-01 20:40:58 +0200

    disable env type checking

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -2,7 +2,7 @@
  * Instantiates a single instance PrismaClient and save it on the global object.
  * @link https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
  */
-import { env } from './env';
+// import { env } from './env';
 import { PrismaClient } from '@prisma/client';
 
 const prismaGlobal = global as typeof global & {
@@ -12,10 +12,5 @@ const prismaGlobal = global as typeof global & {
 export const prisma: PrismaClient =
   prismaGlobal.prisma ||
   new PrismaClient({
-    log:
-      env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    log: ['error'],
   });
-
-if (env.NODE_ENV !== 'production') {
-  prismaGlobal.prisma = prisma;
-}
