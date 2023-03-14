11160df12095a250ade7d12cec163abb24e9d033
Awelani Murovhi <u18335412@tuks.co.za>
2023-03-14 19:50:43 +0200

    remove comments

--- src/server/routers/column.ts
+++ src/server/routers/column.ts
@@ -12,7 +12,7 @@ const defaultColumnSelect = Prisma.validator<Prisma.ColumnSelect>()({
       Sub_Task: true,
     },
   },
-//   task_order: true,
+  //   task_order: true,
 });
 
 export const columnRouter = createRouter()
@@ -85,25 +85,4 @@ export const columnRouter = createRouter()
         id,
       };
     },
-  })
-//   .mutation('updateOrder', {
-//     input: z.object({
-//       id: z.string().uuid(),
-//       data: z.object({
-//         order: z.array(z.string()),
-//       }),
-//     }),
-//     async resolve({ input }) {
-//       const {
-//         id,
-//         data: { order },
-//       } = input;
-//       return prisma.column.update({
-//         where: { id },
-//         data: {
-//           task_order: order,
-//         },
-//         select: defaultColumnSelect,
-//       });
-//     },
-//   });
+  });
